27a1e6ee9fffaf830c80afa4d99e5bbc95357f06
tomike19 <kolajoelizabeth@gmail.com>
2022-08-15 23:07:38 +0100

    string methods

---
+++ Model Data/index.js
@@ -0,0 +1,12 @@
+//string methods
+
+let message = "You are learning really fast"
+message=message.toUpperCase()
+console.log(message)
+
+
+//Trim methods
+
+let color = "     black   is   gold     ";
+color = color.trim.toUpperCase()
+console.log(color)
