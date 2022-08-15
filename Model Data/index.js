acd8322a0bb7329558f360475a1b1761cfbfd260
tomike19 <kolajoelizabeth@gmail.com>
2022-08-15 23:28:01 +0100

    string methods

--- Model Data/index.js
+++ Model Data/index.js
@@ -1,12 +1,27 @@
 //string methods
-
 let message = "You are learning really fast"
-message=message.toUpperCase()
+message = message.toUpperCase()
 console.log(message)
 
-
 //Trim methods
-
 let color = "     black   is   gold     ";
-color = color.trim.toUpperCase()
+color = color.trim()
 console.log(color)
+
+//Index of method
+let games = "basketball"
+games = games.indexOf(1)
+console.log(games)
+
+//slice methods
+//   It takes the slice of an  existing string and it gives a piece of it.
+
+let sport = "Baseball"
+sport = sport.slice(2,4)
+console.log(sport)
+
+//Rreplace method
+
+let entertainment = "entertain";
+entertainment = entertainment.replace('entertain', 'enjoyment')
+console.log(entertainment)
