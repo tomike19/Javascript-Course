9706d4545859d928b9d207984683de1d9dd959f8
tomike19 <kolajoelizabeth@gmail.com>
2022-08-24 00:01:46 +0100

    conditional statement

--- Controlling Program Logic/index.js
+++ Controlling Program Logic/index.js
@@ -5,11 +5,32 @@ if (1 === 1) {
 }
 
 
-let rating = 2;
+let rating = 0;
 
 if (rating === 3) {
     console.log("It is a goal")
 }
-else if(rating === 2) {
+else if (rating === 2) {
     console.log("MEETS EXPECTATION")
 }
+else if (rating === 5) {
+    console.log("You are too over a score")
+}
+else {
+    console.log("You are not fit")
+}
+
+function findLargest(num1, num2) {
+    if (num1 > num2) {
+        console.log(`${num1} is the greatest number`)
+    }
+
+    else if (num1 < num2) {
+        console.log(`${num1} is the largest`)
+    }
+    else {
+        console.log(`${num1} is equal to ${num2}`)
+    }
+
+}
+    findLargest(45,45)
